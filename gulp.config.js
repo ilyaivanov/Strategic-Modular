15f84d2598a90109049e98127f33975fea226451
Ilya Ivanov <static.ila@gmail.com>
2015-07-18 14:35:55 +0300

    Finished with wiredep, running tests and gulp

--- gulp.config.js
+++ gulp.config.js
@@ -1,12 +1,27 @@
 module.exports = function () {
-    var config = {};
-    config.getKarmaOptions = getKarmaOptions;
+    return {
+        alljs: [
+            "src/*.js",
+            "src/app/common.module.js",
+            "src/app/common/models.module.js",
+            "src/app/**/*.js",
+            "!src/app/**/*spec.js"
+        ],
+        css: ['src/css/*.css'],
+        index: 'src/main.html',
+        wiredepOptions: {
+            json: require('./bower.json'),
+            directory: './bower_components',
+            ignorePath: '../'
+        },
 
-    return config;
+        injectOptions: {addRootSlash: false},
+        getKarmaOptions: getKarmaOptions
+    };
 
     function getKarmaOptions() {
         return {
-            files:  [
+            files: [
                 "bower_components/jquery/dist/jquery.js",
                 "bower_components/angular/angular.js",
                 "bower_components/angular-mocks/angular-mocks.js",
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,63 @@
 var gulp = require('gulp');
-var karma = require('gulp-karma');
+var $ = require('gulp-load-plugins')({lazy: true});
 var config = require('./gulp.config')();
+var wiredep = require('wiredep').stream;
+var args = require('yargs').argv;
 
 
+gulp.task('default', ['inject'], function () {
+    log('\n\nBuild OK');
+});
 
-gulp.task('test', function() {
+
+gulp.task('inject', ['test'], function () {
+    log('Wire up the bower css, js and our app css, js into the html');
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(config.wiredepOptions))
+        .pipe($.inject(gulp.src(config.alljs).pipe($.if(args.verbose, $.print())), config.injectOptions))
+        .pipe($.inject(gulp.src(config.css), config.injectOptions))
+        .pipe($.rename('index.html'))
+        .pipe(gulp.dest(''));
+});
+
+
+gulp.task('test', function () {
+    log('Running unit tests');
     return gulp.src(config.getKarmaOptions().files)
-        .pipe(karma({
+        .pipe($.karma({
             configFile: 'karma.conf.js',
             action: 'run'
         }))
-        .on('error', function(err) {
+        .on('error', function (err) {
             throw err;
         });
 });
 
 
-gulp.task('default', ['test'], function () {
-    console.log('\n\nBuild OK');
+//now is ignored
+gulp.task('vet', function() {
+    log('Analyzing source with JSHint and JSCS');
+
+    return gulp
+        .src(config.alljs)
+        .pipe($.if(args.verbose, $.print()))
+        .pipe($.jscs())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('fail'));
 });
 
 
+function log(msg) {
+    if (typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                $.util.log($.util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        $.util.log($.util.colors.blue(msg));
+    }
+}
