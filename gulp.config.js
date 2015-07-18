2dbb3af97f39d6c582ae8bbe2e2c182b63ec4d9d
Ilya Ivanov <static.ila@gmail.com>
2015-07-18 13:44:28 +0300

    Moved dependencies from npm to bower

--- gulp.config.js
+++ gulp.config.js
@@ -7,10 +7,10 @@ module.exports = function () {
     function getKarmaOptions() {
         return {
             files:  [
-                "node_modules/angular/angular.js",
                 "bower_components/jquery/dist/jquery.js",
-                "node_modules/angular-mocks/angular-mocks.js",
-                "node_modules/ui-router/angular-ui-router.js",
+                "bower_components/angular/angular.js",
+                "bower_components/angular-mocks/angular-mocks.js",
+                "bower_components/ui-router/release/angular-ui-router.js",
                 "src/*.js",
                 "src/app/common.module.js",
                 "src/app/common/models.module.js",
